2edda020f144a455cb8b479fdfcf2bcfe54d4ba8
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2024-01-09 21:33:46 +0200

    Add more validations

--- src/bg/restaurant/systems/software/integration/design/command/CommandExecutor.java
+++ src/bg/restaurant/systems/software/integration/design/command/CommandExecutor.java
@@ -6,27 +6,36 @@ import bg.restaurant.systems.software.integration.design.command.handlers.file.G
 import bg.restaurant.systems.software.integration.design.command.handlers.preparation.GetPreparationTimeForRecipeByNameCommand;
 import bg.restaurant.systems.software.integration.design.command.handlers.recipe.RecipeCommandExecutor;
 import bg.restaurant.systems.software.integration.design.command.handlers.serving.GetServingWayByRecipeNameCommand;
+import bg.restaurant.systems.software.integration.design.command.validators.CommandExecutorValidator;
 import bg.restaurant.systems.software.integration.design.storage.CocktailStorage;
 
-public class CommandExecutor {
+public class CommandExecutor extends CommandExecutorValidator {
     private final CocktailStorage cocktailStorage;
 
     public CommandExecutor(CocktailStorage cocktailStorage) {
+        validateConstructorParameter(cocktailStorage);
+
         this.cocktailStorage = cocktailStorage;
     }
 
     public String execute(Command cmd) {
-        CommandType commandType = CommandType.getCommandType(cmd.command());
+        try {
+            validateCommand(cmd);
 
-        return switch (commandType) {
-            case RECIPES -> new RecipeCommandExecutor(cocktailStorage).execute(cmd.arguments());
-            case DRINKS -> new DrinkCommandExecutor(cocktailStorage).execute(cmd.arguments());
-            case FILE -> new GetFileWithRecipeCommand(cocktailStorage, cmd.arguments()).execute();
-            case ALLERGENS -> new GetAllergensByRecipeNameCommand(cocktailStorage, cmd.arguments()).execute();
-            case PREPARATION_TIME ->
-                new GetPreparationTimeForRecipeByNameCommand(cocktailStorage, cmd.arguments()).execute();
-            case SERVE_WAY -> new GetServingWayByRecipeNameCommand(cocktailStorage, cmd.arguments()).execute();
-            default -> "Unknown command";
-        };
+            CommandType commandType = CommandType.getCommandType(cmd.command());
+
+            return switch (commandType) {
+                case RECIPES -> new RecipeCommandExecutor(cocktailStorage).execute(cmd.arguments());
+                case DRINKS -> new DrinkCommandExecutor(cocktailStorage).execute(cmd.arguments());
+                case FILE -> new GetFileWithRecipeCommand(cocktailStorage, cmd.arguments()).execute();
+                case ALLERGENS -> new GetAllergensByRecipeNameCommand(cocktailStorage, cmd.arguments()).execute();
+                case PREPARATION_TIME ->
+                    new GetPreparationTimeForRecipeByNameCommand(cocktailStorage, cmd.arguments()).execute();
+                case SERVE_WAY -> new GetServingWayByRecipeNameCommand(cocktailStorage, cmd.arguments()).execute();
+                default -> "Unknown command";
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 }
