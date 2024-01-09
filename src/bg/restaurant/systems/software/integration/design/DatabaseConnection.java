a660e610431e02e0a87c86b2d3aacb335d2d0e4a
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2024-01-09 23:49:27 +0200

    Fix conflicts

---
+++ src/bg/restaurant/systems/software/integration/design/DatabaseConnection.java
@@ -0,0 +1,65 @@
+package bg.restaurant.systems.software.integration.design;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    private static final String CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+
+    public DatabaseConnection(String url, String user, String password) {
+        this.connection = null;
+
+        try {
+            Class.forName(CLASS_NAME);
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public ResultSet executeQuery(String query) {
+        try {
+            resultSet = statement.executeQuery(query);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return resultSet;
+    }
+
+    public void closeConnectionsToDatabase() {
+        try {
+            statement.close();
+            connection.close();
+            resultSet.close();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void main(String[] arg) {
+        DatabaseConnection databaseConnection = new DatabaseConnection(
+                "jdbc:mysql://localhost:3306/restaurants", "root", "");
+
+        try {
+            ResultSet resultSet = databaseConnection.executeQuery("select * from recipes");
+
+            int id;
+            String name;
+            String type;
+            while (resultSet.next()) {
+                id = resultSet.getInt("id");
+                name = resultSet.getString("name");
+                type = resultSet.getString("type");
+                System.out.println("Id : " + id
+                        + " Name : " + name + " " + type);
+            }
+        } catch (
+                Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
